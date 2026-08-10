12267f78f100e01643dff84afa60fb8e4c06fb47
Adnan Memic <adnan.memic27@gmail.com>
2026-08-10 16:44:27 +0200

    feat: add colums id and description

--- main.py
+++ main.py
@@ -8,15 +8,24 @@ FILE_PATH = Path("data.csv")
 # Fileexistence check and file empty check
 def initialize_csv():
     if not FILE_PATH.is_file() or FILE_PATH.stat().st_size == 0:
-        df = pd.DataFrame(columns=["account", "money", "date"])
+        df = pd.DataFrame(columns=["id", "account", "money", "date", "description"])
         df.to_csv(FILE_PATH, header=True, index=False)
 
 # Add new entry
-def add_money(amount):
-    df = pd.DataFrame([
-        {"account": "bank", "money": amount, "date": date.today().isoformat()}
-    ])
+def add_money(amount, description):
+    df_csv = pd.read_csv(FILE_PATH)
+    new_id = 1 if df_csv.empty else df_csv["id"].max() + 1
+
+    df = pd.DataFrame([{
+        "id": new_id,
+        "account": "bank", 
+        "money": amount, 
+        "date": date.today().isoformat(),
+        "description": description
+    }])
     df.to_csv(FILE_PATH, mode="a", header=False, index=False)
+    print(f"Added: \n{df}")
+
 
 # arguments check: python3 main.py <mode> <amount>
 if len(sys.argv) < 3:
