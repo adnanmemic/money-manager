0828fd0aca544c5b1767fad2edf8e8175ba5f2e0
Adnan Memic <adnan.memic27@gmail.com>
2026-08-11 23:13:52 +0200

    feat: add remove function

--- main.py
+++ main.py
@@ -27,16 +27,33 @@ def add_money(amount, description):
     df.to_csv(FILE_PATH, mode="a", header=False, index=False)
     print(f"Added: \n{df}")
 
+# Remove transaction by the id
+def remove_transaction(id):
+    df = pd.read_csv(FILE_PATH)
+
+    # check id before removing
+    df_removed = df[df["id"] == id] # Show which row was removed, before removing it
+    if df_removed.empty:
+        print(f"Error: Transaction with ID {id} not found")
+        return
+    print(f"Removed: \n{df_removed}")
+
+    df = df[df["id"] != id] # to remove the row 
+    df.to_csv(FILE_PATH, header=True, index=False)
+
+# Commands
 parser = argparse.ArgumentParser()
-
 subparsers = parser.add_subparsers(dest="command")
-
+# Subcommand: add
 add_parser = subparsers.add_parser("add", help="add a new transaction")
 add_parser.add_argument("amount", type=float, help="amount of money (also negative)")
 add_parser.add_argument( "-d", "--description",
                     default="No description", 
                     help="add a description to the transaction"
                     )
+# Subcommand: remove
+remove_parser = subparsers.add_parser("remove", help="remove a transaction")
+remove_parser.add_argument("id", type=int, help="id of the transaction")
 
 args = parser.parse_args()
 
@@ -46,6 +63,8 @@ initialize_csv()
 
 if args.command == "add":
     add_money(args.amount, args.description) 
+elif args.command == "remove":
+    remove_transaction(args.id)
 else:
     parser.print_help()
     sys.exit(0)
